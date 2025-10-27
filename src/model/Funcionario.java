85d9b4bde31e7af1428e6efdce6c84d970b4c08c
Johnn <jhonathan.magalhaes@sempreceub.com>
2025-10-27 14:27:48 -0300

    Update Funcionario.java

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,16 +1,24 @@
-// src/model/Funcionario.java
 package model;
 
 public class Funcionario {
-    private int id_funcionario; // Nome do SQL
+    private int id_funcionario; // ID do banco
     private String nome;
-    private String cargo;
-    private String telefone;
+    private double salario;
+    private String matricula;
 
-    public Funcionario(String nome, String cargo, String telefone) {
+    // 1. Construtor para CRIAR um novo funcionário (INSERT no banco - SEM ID)
+    public Funcionario(String nome, double salario, String matricula) {
         this.nome = nome;
-        this.cargo = cargo;
-        this.telefone = telefone;
+        this.salario = salario;
+        this.matricula = matricula;
+    }
+
+    // 2. Construtor para LER do banco de dados (SELECT - COM ID)
+    public Funcionario(int id_funcionario, String nome, double salario, String matricula) {
+        this.id_funcionario = id_funcionario; // Atribui o ID lido do banco
+        this.nome = nome;
+        this.salario = salario;
+        this.matricula = matricula;
     }
 
     // Getters e Setters
@@ -18,13 +26,13 @@ public class Funcionario {
     public void setId(int id) { this.id_funcionario = id; }
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
-    public String getCargo() { return cargo; }
-    public void setCargo(String cargo) { this.cargo = cargo; }
-    public String getTelefone() { return telefone; }
-    public void setTelefone(String telefone) { this.telefone = telefone; }
+    public double getSalario() { return salario; }
+    public void setSalario(double salario) { this.salario = salario; }
+    public String getMatricula() { return matricula; }
+    public void setMatricula(String matricula) { this.matricula = matricula; }
 
     @Override
     public String toString() {
-        return "Funcionario [ID=" + id_funcionario + ", Nome='" + nome + "', Cargo='" + cargo + "', Telefone='" + telefone + "']";
+        return "Funcionario [ID=" + id_funcionario + ", Nome='" + nome + "', Salario=" + salario + ", Matrícula='" + matricula + "']";
     }
 }
